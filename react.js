0d280b0af6af2a268abf308106e4114ef873cace
LuckyLM9 <119536337+LuckyLM9@users.noreply.github.com>
2023-04-13 18:04:43 +0200

    Add files via upload

---
+++ react.js
@@ -0,0 +1,13 @@
+fetch('https://striveschool-api.herokuapp.com/api/product/')
+   .then(response => response.json())
+   .then(data => console.log(data));
+export const labelContainer =({}) => {
+    return }
+    <div className="item-add">
+        <input
+        id={id}
+        name={id}
+        type="listbox"
+    />
+    </div>
+        
